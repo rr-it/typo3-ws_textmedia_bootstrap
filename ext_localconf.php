f4b4a759d6465cf1346e8eb13f1a074239dd990c
Ayke Halder <rr-it@users.noreply.github.com>
2024-08-12 18:59:15 +0200

    Fix deprecation: #92947 - TYPO3_MODE and TYPO3_REQUESTTYPE constants

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,25 +1,8 @@
 <?php
-if (!defined('TYPO3_MODE')) {
+if (!defined('TYPO3')) {
     die('Access denied.');
 }
 
 // Register for hook to show preview of tt_content element of CType="wstextmediabootstrap" in page module
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem']['wstextmediabootstrap']
     = Websight\WsTextmediaBootstrap\Preview\WsTextmediaPreviewRenderer::class;
-
-if (TYPO3_MODE === 'BE') {
-    call_user_func(
-        function ($extKey) {
-            // Get the extension configuration
-            $extConfLoadContentElementWizardTsConfig = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
-                \TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class
-            )->get($extKey, 'loadContentElementWizardTsConfig');
-
-            if ((int)$extConfLoadContentElementWizardTsConfig === 1) {
-                // Include new content elements to modWizards
-                \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:ws_textmedia_bootstrap/Configuration/PageTSconfig/NewContentElementWizard.typoscript">');
-            }
-        },
-        'ws_textmedia_bootstrap'
-    );
-}
