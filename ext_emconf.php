32e25670f26844faec4b7d8c6cfe6da7d9211d7f
Ayke Halder <rr-it@users.noreply.github.com>
2025-03-24 15:04:27 +0000

    Update deprecated `ext_emconf.php`: set dependency to TYPO3 v12 and clear all other constraints

--- ext_emconf.php
+++ ext_emconf.php
@@ -16,9 +16,7 @@ $EM_CONF[$_EXTKEY] = [
     'version'          => '2.1.2',
     'constraints'      => [
         'depends'   => [
-            'typo3' => '10.4.0-10.4.99',
-            'fluid_styled_content' => '10.4.0-10.4.99',
-            'responsive_images' => '2.0.0-2.99.99',
+            'typo3' => '12.4.0-12.4.99',
         ],
         'conflicts' => [],
         'suggests'  => [],
